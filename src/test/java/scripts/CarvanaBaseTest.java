0bd1166f5100b4953b1efdf400733ba744ffcdb3
Abdallah Jaber <abdallahj708@gmail.com>
2023-02-07 13:57:21 -0600

    updated Project3

--- src/test/java/scripts/CarvanaBaseTest.java
+++ src/test/java/scripts/CarvanaBaseTest.java
@@ -11,7 +11,6 @@ public class CarvanaBaseTest {
 
     CarvanaBaseTest carvanaBaseTest;
     CarvanaBasePage carvanaBasePage;
-    CarvanaTestCases carvanaTestCases;
     WebDriver driver;
 
     @BeforeMethod
